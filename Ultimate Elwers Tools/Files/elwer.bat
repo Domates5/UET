228c6e240dec1e41caa821945c023f1552aef228
Domates5 <85279357+Domates5@users.noreply.github.com>
2024-07-31 10:27:18 +0300

    Add files via upload

---
+++ Ultimate Elwers Tools/Files/elwer.bat
@@ -0,0 +1,426 @@
+REM              SETUP
+@echo off
+TITLE Elwer's Account Manager
+color 0f
+
+
+chcp 65001 >nul
+cls
+
+
+:Selection
+CALL:LOGO
+echo]
+echo]
+echo [90m^>	[97m1. Ghost Of Tsushima
+echo [90m^>	[97m2. The Last Of Us Part 1
+echo [90m^>	[97m3. Elder Ring
+echo [90m^>	[97m4. Cyberpunk 2077
+echo [90m^>	[97m5. Gas Station Sim
+echo [90m^>	[97m6. Spiderman Miles Morales
+echo [90m^>	[97m7. Stray
+echo [90m^>	[97m8. Sifu
+echo [90m^>	[97m9. Witcher 3
+echo [90m^>	[97m10. Spiderman Remastered
+echo [90m^>	[97m11. God Of War
+echo [90m^>	[97m12. Red Dead Redemption 2(you need Nightlight Launcher)
+echo [90m^>	[97m13. Grand Theft Auto 5(you need Nightlight Launcher)
+echo [90m^>	[97m14. Forza horizon 5
+echo [90m^>	[97m15. Detroit Become a Human
+echo [90m^>	[97m16. Resident Evil 3/7/Village
+echo [90m^>	[97m17. Resident Evil 4 remastered
+echo [90m^>	[97m18. Dying Light 2
+echo [90m^>	[97m19. Mortal Kombat 1
+echo [90m^>	[97m20. Hades II
+echo [90m^>	[97m21. Uncharted Legacy Of Thieves Collection
+echo [90m^>	[97m22. Hogwarts Legacy
+echo [90m^>	[97m23. FC 24
+echo [90m^>	[97m24. Sekiro™: Shadows Die Twice
+echo [90m^>	[97m25. Outlast 1-2
+echo [90m^>	[97m26. Teardown
+echo [90m^>	[97m27. Tekken 8	
+echo [90m^>	[97m28. Jujutsu Kaisen Cursed Clash
+echo [90m^>	[97m29. No Man's Sky
+echo [90m^>	[97m30. Ready Or Not
+echo [90m^>	[97m31. It takes two (use parsec)
+echo [90m^>	[97m32. Celeste 
+echo [90m^>	[97m32. Fnaf All Games
+echo]
+
+SET /P selection=
+
+IF /I '%selection%'=='1' GOTO Acc1
+IF /I '%selection%'=='2' GOTO Acc2
+IF /I '%selection%'=='3' GOTO Acc3
+IF /I '%selection%'=='4' GOTO Acc4
+IF /I '%selection%'=='5' GOTO Acc5
+IF /I '%selection%'=='6' GOTO Acc6
+IF /I '%selection%'=='7' GOTO Acc7
+IF /I '%selection%'=='8' GOTO Acc8
+IF /I '%selection%'=='9' GOTO Acc9
+IF /I '%selection%'=='10' GOTO Acc10
+IF /I '%selection%'=='11' GOTO Acc11
+IF /I '%selection%'=='12' GOTO Acc12
+IF /I '%selection%'=='13' GOTO Acc13
+IF /I '%selection%'=='14' GOTO Acc14
+IF /I '%selection%'=='15' GOTO Acc15 
+IF /I '%selection%'=='16' GOTO Acc16
+IF /I '%selection%'=='17' GOTO Acc17
+IF /I '%selection%'=='18' GOTO Acc18
+IF /I '%selection%'=='19' GOTO Acc19
+IF /I '%selection%'=='20' GOTO Acc20
+IF /I '%selection%'=='21' GOTO Acc21
+IF /I '%selection%'=='22' GOTO Acc22
+IF /I '%selection%'=='23' GOTO Acc23
+IF /I '%selection%'=='24' GOTO Acc24
+IF /I '%selection%'=='25' GOTO Acc25
+IF /I '%selection%'=='26' GOTO Acc26
+IF /I '%selection%'=='27' GOTO Acc27
+IF /I '%selection%'=='28' GOTO Acc28
+IF /I '%selection%'=='29' GOTO Acc29
+IF /I '%selection%'=='30' GOTO Acc30
+IF /I '%selection%'=='31' GOTO Acc31
+IF /I '%selection%'=='32' GOTO Acc32
+IF /I '%selection%'=='33' GOTO Acc33
+
+:Acc1
+echo|set /p=wkg8542 pass:Bcp827643|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m1. Ghost Of Tsushima [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc2
+echo|set /p=steamok1090243 pass:steamok45678912|clip
+cls
+CALL :LOGO
+echo]
+echo]
+echo [90m^>	[97m2. The Last Of Us Part 1 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc3
+echo|set /p=wbtq108237 pass:steamok139461|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m3. Elder Ring [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc4
+echo|set /p=ui1vl1fe2ev3 pass:43895546tt|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m4. Cyberpunk 2077 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc5
+echo|set /p=zqcdd33343 pass:q7808708|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m5. Gas Station Sim [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc6
+echo|set /p=31771114 pass:40827578|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m6. Spiderman Miles Morales [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc7
+echo|set /p=jluwu557 pass:fjmu71753GX|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m7. Stray [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc8
+echo|set /p=wbtq1090746 pass:steamok59558843|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m8. Sifu [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc9
+echo|set /p=wbtq1088884 pass:wbtq1088884|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m9. Witcher 3 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc10
+echo|set /p=teq8084 pass:Dbk6554545|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m10. Spiderman Remastered [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc11
+echo|set /p=ndriq606 pass:xxdanji115|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m11. God Of War [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc12
+echo|set /p=enyugame2913 pass:enyugame8578|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m12. Red Dead Redemption 2 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc13
+echo|set /p=Not Avaible|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m13. Grand Theft Auto 5 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc14
+echo|set /p=72288699 pass:class42513class|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m14. Forza Horizon 5 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc15
+echo|set /p=cwbpp57700 pass:Steamok878945|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m15. Detroit Become a Human [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc16
+echo|set /p=cwbpp57700 pass:Steamok878945|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m16. Resident Evil 3/7/Village [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc17
+echo|set /p=Not Avaible|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m17. Resident Evil 4 remake [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc18
+echo|set /p=3brogamersdyinglightstd31 pass:NCSaveWaterBroBG_DLsdd423|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m18. Dying Light 2 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc19
+echo|set /p=bullzarasi1989 pass:nG6jEJx1U51984|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m19. Mortal Kombat 1 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc20
+echo|set /p=vpjfe36149 pass:troj16837P|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m20. Hades II [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc21
+echo|set /p=wbtq1089845 pass:steamok1089845|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m21. Uncharted Legacy Of Thieves Collection [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc22
+echo|set /p=Maksimqb1 pass:Anpol2005.|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m22. Hogwarts Legacy [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc23
+echo|set /p=Maksimqb1 pass:Anpol2005.|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m23. FC 24 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc24
+echo|set /p=thb112361 pass:steamok112361|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m24. Sekiro: Shadows Die Twice [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc25
+echo|set /p=Not Avaible|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m25. Outlast 1 & 2 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc26
+echo|set /p=sq9wul pass:junyi1122016|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m26. Teardown [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc27
+echo|set /p=legatebroke pass:WPqL5HPk|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m27. Tekken 8 [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc28
+echo|set /p=legatebroke pass:WPqL5HPk|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m28. Jujutsu Kaisen Cursed Clash [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc29
+echo|set /p=alina4501 pass:rye6436r|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m29. No Man's Sky [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc30
+echo|set /p=wbtq1086101 pass:steamok987789|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m30. Ready Or Not [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc31
+echo|set /p=Maksimqb1 pass:Anpol2005.|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m31. It takes two [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc32
+echo|set /p=jjk9485902 pass:huanxiang1106|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m32. Celeste [92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+:Acc33
+echo|set /p=teknikengin pass:qQGEujoS|clip
+cls
+CALL :LOGO	
+echo]
+echo]
+echo [90m^>	[97m33. Fnaf All Games[92m[Credentials Copied]
+ping localhost -n 2 >nul
+exit /b
+
+
+
+
+
+
+
+REM				LOGO
+
+:LOGO
+
+call logo.bat
